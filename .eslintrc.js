0b82800c0d27b6b31b6b6f6b3fe4e050609ae60b
Emre Reyhan <ereyhan60@gmail.com>
2023-07-07 14:04:42 +0300

    getOne

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'only-warn'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
--- src/movie/movie.repository.ts
+++ src/movie/movie.repository.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { crudInterface } from 'src/common/crud.interface';
 import { Movie } from './entities/movie.entity';
 import { Neo4jService } from 'nest-neo4j';
+import { error } from 'console';
 
 @Injectable()
 export class movieRepository implements crudInterface<Movie> {
-  constructor(private readonly neo4jService: Neo4jService) {}
+  constructor(private readonly neo4jService: Neo4jService) { }
   async getAll(): Promise<Movie[]> {
     const res = await this.neo4jService.read(`MATCH (n:Movie) RETURN n`);
     const result = [];
@@ -34,12 +35,12 @@ export class movieRepository implements crudInterface<Movie> {
           return director;
         }),
         actors: actorsQuery.records.map((record) => {
-            let actor = {
-                id: record.get('m').identity.toInt(),
-                ...record.get('m').properties,
-            };
-            if (actor.born) actor.born = actor.born.toInt();
-            return actor;
+          let actor = {
+            id: record.get('m').identity.toInt(),
+            ...record.get('m').properties,
+          };
+          if (actor.born) actor.born = actor.born.toInt();
+          return actor;
         }),
       });
     }
@@ -47,12 +48,46 @@ export class movieRepository implements crudInterface<Movie> {
     return result;
   }
   async getOne(id: number): Promise<Movie> {
-    return {
-      id,
-      title: 'test',
-      released: 2021,
-      tagline: 'test',
-    };
+    const res = await this.neo4jService.read(`MATCH (n:Movie) where id(n)=${id} RETURN n`);
+    const result = [];
+    if (!res.records.length)
+      throw new HttpException('HATA', HttpStatus.BAD_REQUEST)
+    console.log(res.records.at(0).get('n').properties.title);
+    const movieProps = res.records.at(0).get('n');
+
+    let movie = {
+      id: id,
+      title: movieProps.properties.title,
+      released: movieProps.properties.released.toInt(),
+      tagline: movieProps.properties.tagline,
+      directors: [],
+      actors: []
+    }
+
+    const directorsQuery = await this.neo4jService.read(
+      `match (n:Movie)<-[:DIRECTED]-(m:Person) where id(n)=${id} return m`,
+    );
+    const actorsQuery = await this.neo4jService.read(
+      `match (n:Movie)<-[:ACTED_IN]-(m:Person) where id(n)=${id} return m`,
+    );
+
+    movie.directors = directorsQuery.records.map((record) => {
+      let director = {
+        id: record.get('m').identity.toInt(),
+        ...record.get('m').properties,
+      };
+      if (director.born) director.born = director.born.toInt();
+      return director;
+    })
+    movie.actors = actorsQuery.records.map((record) => {
+      let actor = {
+        id: record.get('m').identity.toInt(),
+        ...record.get('m').properties,
+      };
+      if (actor.born) actor.born = actor.born.toInt();
+      return actor;
+    })
+    return movie;
   }
   create(t: Movie): Movie {
     return new Movie();
